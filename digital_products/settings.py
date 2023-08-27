8d6efc81a83f4ecaf98a93b41e8bed006c11313f
parsadox2 <parsa.ravaghi@gmail.com>
2023-08-28 00:55:37 +0330

    commit

--- digital_products/settings.py
+++ digital_products/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -114,6 +115,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+
+#media files
+MEDIA_ROOT = os.path.join(BASE_DIR,'/media/')
+MEDIA_URL = '/media/'
+
+
 
 STATIC_URL = 'static/'
 
--- products/models.py
+++ products/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-from django.utils.translation import ungettext_lazy as _
-# Create your models here.
+from django.utils.translation import gettext_lazy as _
+# hello hello world
+
+
+
 
 class Category(models.Model):
     parent = models.ForeignKey('self',verbose_name=_('parent'),blank=True,null=True,on_delete=models.CASCADE)
@@ -16,6 +19,7 @@ class Category(models.Model):
         db_table = 'categories'
         verbose_name = _('Category')
         verbose_name_plural = _('Categories')
+
 
 
 
@@ -35,4 +39,14 @@ class Product(models.Model):
 
 
 class File(models.Model):
-    pass
+    product = models.ForeignKey("Product", verbose_name=_("product"), on_delete=models.CASCADE)
+    title = models.CharField(_('title'),max_length=50)
+    file = models.FileField(_("file"), upload_to='files/%y/%m/%d')
+    is_enable = models.BooleanField(_('is_enable'))
+    created_time = models.DateTimeField(_('created_time'),auto_now_add=True)
+    updated_time = models.DateTimeField(_('updated_time'),auto_now=True)
+    class Meta:
+        db_table = 'files'
+        verbose_name = _('Files')
+        verbose_name_plural = _('files')
+    
